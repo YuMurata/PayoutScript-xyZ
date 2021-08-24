929e98002f3dc48c1562c6139133d831dcaec201
purin52002 <purin7635@gmail.com>
2021-08-25 00:22:44 +0900

    [追加] slp-payout-config.jsonを初期化する処理を追加

--- setup.sh
+++ setup.sh
@@ -11,3 +11,5 @@ wsl sudo apt install -y python3 python3-pip
 
 wsl pip install -U pip
 wsl pip install -r requirements.txt
+
+cp ${SCRIPT_DIR}/slp-payout-config.json.template ${SCRIPT_DIR}/slp-payout-config.json
