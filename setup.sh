79262f2664c160e7529492d203a7a58bb6683298
purin52002 <purin7635@gmail.com>
2021-08-25 00:20:46 +0900

    [追加] 仮想linuxをセットアップするスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+wsl sudo apt update
+wsl sudo apt -y upgrade
+
+wsl sudo apt install -y python3 python3-pip
+
+wsl pip install -U pip
+wsl pip install -r requirements.txt
