ec8a627526cb6be102a028fadc1f9352701c3be7
purin52002 <purin7635@gmail.com>
2021-08-26 00:38:16 +0900

    [修正] buttonをButtonFrameにまとめた

--- GUI/scholars/scholars_frame.py
+++ GUI/scholars/scholars_frame.py
@@ -1,11 +1,33 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from typing import Tuple
+from typing import Callable, Tuple
 from info import PayoutInfo, ScholarInfo
 from .edit_frame import EditWindow
 
 
+class ButtonFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.edit_button = ttk.Button(self, text='edit')
+        self.edit_button.pack(side=tk.LEFT)
+
+
+
+    def bind_edit_button(self, selection_func: Callable[[], Tuple[str]],
+                         load_payout_func: Callable[[], None]):
+        def open_window():
+            selection_list = selection_func()
+            if len(selection_list) < 1:
+                messagebox.showerror('error', 'select scholar')
+                return
+
+            scholar_index = int(selection_list[0])
+            window = EditWindow(self, scholar_index)
+            window.bind_func(load_payout_func)
+
+        self.edit_button.bind('<Button-1>', lambda e: open_window())
 class ScholarsFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
@@ -16,20 +38,9 @@ class ScholarsFrame(tk.Frame):
         self.item_id_list: Tuple[str] = tuple([])
         self.load_payout()
 
-        edit_button = ttk.Button(self, text='edit')
-        edit_button.pack(padx=10, pady=10)
-
-        def open_edit_window():
-            selection_list = self.tree.selection()
-            if len(selection_list) < 1:
-                messagebox.showerror('error', 'select scholar')
-                return
-
-            scholar_index = int(selection_list[0])
-            window = EditWindow(self, scholar_index)
-            window.bind_func(self.load_payout)
-
-        edit_button.bind('<Button-1>', lambda e: open_edit_window())
+        button_frame = ButtonFrame(self)
+        button_frame.bind_edit_button(self.tree.selection, self.load_payout)
+        button_frame.pack(padx=10, pady=10)
 
     def create_tree(self) -> ttk.Treeview:
         columns = tuple(ScholarInfo._fields)
